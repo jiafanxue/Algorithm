bdd4b9061964dca84cc3ca094e63b990cbe0c1a0
Tutuxsfly <Tutuxsfly@126.com>
2017-01-18 20:48:09 +0800

    Add BeautyOfProgramming/Chapter 2/lengthestIncrease.cpp

---
+++ BeautyOfProgramming/Chapter 2/lengthestIncrease.cpp
@@ -0,0 +1,14 @@
+/*
+Question:
+	Write a simple program, that can find a one-dimensional subarray which is lengthest of increase subarray.
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
