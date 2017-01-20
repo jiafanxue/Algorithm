1fee82381c045e2348e0573ade5a53cf6b87aca0
Tutuxsfly <Tutuxsfly@126.com>
2017-01-20 22:49:00 +0800

    Add BeautyOfProgramming/Chapter 2/IntervalCoincidenceDec.app

---
+++ BeautyOfProgramming/Chapter 2/IntervalCoincidenceDec.cpp
@@ -0,0 +1,27 @@
+/*
+Question:
+	Give a source interval [x,y] and N unordered target intervals[x1, y1], [x2, y2], ..., [xn, yn]
+	it is determined whether the source interval [x, y] interval belongs to the target range.
+
+	e.g.
+		source interval : [1, 6]
+		unordered intervals : [2, 3], [1, 2], [3, 9]
+
+		yes!
+*/
+#include <iostream>
+using namespace std;
+
+/*
+	Answer one
+*/
+bool IntervalCoinindenceDec()
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
